8437453e87aa33997e5906a814f80e97f719ddc9
Marcin Stefaniak <marcin.stefaniak@plentific.com>
2023-10-30 17:05:44 +0100

    rest

--- .github/actions/test/test.js
+++ .github/actions/test/test.js
@@ -1,14 +1,13 @@
 const fs = require('fs')
 const os = require('os')
 
-const result = 'XD'
-// {
-//   name: 'test',
-//   value: 'test'
-// }
+const result = {
+  name: 'test',
+  value: 'test'
+}
 
 const filePath = process.env.GITHUB_OUTPUT
-const delimiter = `ghadelimiter`
+const delimiter = `ghadelimiter_123e4567-e89b-12d3-a456-426614174000`
 const encoded = `result<<${delimiter}${os.EOL}${JSON.stringify(result)}${os.EOL}${delimiter}${os.EOL}`
 console.log('filePath', filePath)
 console.log(encoded)
